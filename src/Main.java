01518ed5bada5315cbb13c7425651c293e09bab0
Farhatyar <farhatyartamim290@gmail.com>
2023-03-28 20:26:08 -0400

    i added the feature sun but there are errors that need to be resolved

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
         solarSystem solar=new solarSystem();
         solar.sun="1";
         solar.moon="2";
         solar.planets="2";
+
+        featureSun Xron= new featureSun();
+        Xron.temperature="12345786k";
+        Xron.radius=12456746552421;
+        Xron.color="pink";
     }
 }
